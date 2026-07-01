4b18351583a8d7ff655abc61951e9586b0967215
thelogic0 <thelogic@gmail.com>
2026-07-01 12:46:44 +0900

    [feature] p18

--- BaseballGame/baseball.cpp
+++ BaseballGame/baseball.cpp
@@ -1,3 +1,11 @@
+#include<stdexcept>
+#include<string>
+
+using namespace std;
+
 class Baseball {
 public:
+	void guess(const string& string) {
+		throw length_error("Must be three letters");
+	}
 };
--- BaseballGame/baseball_test.cpp
+++ BaseballGame/baseball_test.cpp
@@ -1,3 +1,9 @@
 #include "gmock/gmock.h"
 #include "baseball.cpp"
+#include "baseball_test.h"
 
+TEST(Baseball, ThrowExceptionWhenInputLengthIsUnmatched)
+{
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12")), length_error);
+}
